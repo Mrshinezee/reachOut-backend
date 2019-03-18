17f258183edabf7ebf293dcefd387f156aab3995
victor <victorugwueze@gmail.com>
2019-03-18 14:35:05 +0100

    chore(linting eslint): configure eslint
    - add airbnb style guide packages
    
    [Finishes #163845122]

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import winston from 'winston';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { ApolloServer} from 'apollo-server';
+import { merge } from 'lodash';
+
 import { loadTypeSchema } from './server/helper/schema';
-import { merge } from 'lodash'
 import user from './server/types/user/user.resolvers';
 
-const types = ['user']
+const types = ['user'];
 
 const start = async () => {
   const rootSchema = `
@@ -15,17 +16,17 @@ const start = async () => {
       query: Query
       mutation: Mutation
     }
-  `
-  const schemaTypes = await Promise.all(types.map(loadTypeSchema))
+  `;
+  const schemaTypes = await Promise.all(types.map(loadTypeSchema));
 
   const server = new ApolloServer({
     typeDefs: [rootSchema, ...schemaTypes],
     resolvers: merge({}, user),
   });
-   
+
   server.listen().then(({ url }) => {
     winston.log('info', `Server ready at ${url}`);
   });
-} 
+};
 
-start()
+start();
--- server/types/user/user.resolvers.js
+++ server/types/user/user.resolvers.js
@@ -1,19 +1,20 @@
-import { User } from '../../models'
+import { User } from '../../models';
 
-const currentUser = (_, args, ctx) => {
 //   return User.findByIdAndUpdate(ctx.user._id, args.input, { new: true })
-  return User.findOne({ where: { email: args.email },})
-}
+const currentUser = (_, args, ctx) => User.findOne({ where: { email: args.email } });
 
-const addUser = (_, args) => {
-  return User.create({ title: 'foo', description: 'bar', deadline: new Date() })
-}
+
+const addUser = (_, args) => User.create({
+  title: 'foo',
+  description: 'bar',
+  deadline: new Date(),
+});
 
 export default {
   Query: {
-    currentUser
+    currentUser,
   },
   Mutation: {
-    addUser
-  }
-}
+    addUser,
+  },
+};
